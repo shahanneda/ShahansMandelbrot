b5e0fa532033e3add74135ca719570718350ea33
Shahan Neda <shahan.neda@gmail.com>
2019-03-30 13:18:49 -0700

    addded storing of old values

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,14 @@
 package Main;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 import ca.shahan.graphics.ViewDisplay;
 
@@ -9,7 +18,7 @@ public class Main {
 	public ViewDisplay panel;
 	long graphicsLastUpdateTime;
 	long timeBetweenGraphicsUpdate = 100;
-	
+	public static int scaleFactor = 5;
 	public static void main(String[] args) {
 		new Main();
 	}
@@ -26,19 +35,169 @@ public class Main {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 		
+//	       InputMap im = panel.getInputMap(JPanel.WHEN_FOCUSED);
+//	        ActionMap am = panel.getActionMap();
+//
+//	        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "onEnter");
+//
+//	        am.put("onEnter", new AbstractAction() {
+//	            @Override
+//	            public void actionPerformed(ActionEvent e) {
+//	                // Enter pressed
+//	            	System.out.println("Enter pressed!");
+//	            	panel.Scale +=100;
+////	            	panel.calcHypno();
+//////	            	panel.clearGui();
+////	            	panel.UpdateGUI();
+////	            	panel.repaint();
+//	            	panel.clearGui();
+//	            }
+//	        });
+		setKeyBindings(panel);
+	      
+
+	        
+	        
 		graphicsLastUpdateTime = System.currentTimeMillis();
+		panel.UpdateGUI();
 		panel.repaint();
-		Loop();
+//		Loop();
 		
 	}
 
+	private void setKeyBindings(JPanel gridPanel) {
+	    InputMap inputMap = 
+	            gridPanel.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW);
+	    inputMap.put(KeyStroke.getKeyStroke("W"), "up arrow");
+	    inputMap.put(KeyStroke.getKeyStroke("Z"), "zoom");
+	    inputMap.put(KeyStroke.getKeyStroke("X"), "dezoom");
+	    inputMap.put(KeyStroke.getKeyStroke("S"), "down arrow");
+	    inputMap.put(KeyStroke.getKeyStroke("A"), "left arrow");
+	    inputMap.put(KeyStroke.getKeyStroke("D"), "right arrow");
 
+	    inputMap.put(KeyStroke.getKeyStroke("UP"), "up arrow");
+	    inputMap.put(KeyStroke.getKeyStroke("DOWN"), "down arrow");
+	    inputMap.put(KeyStroke.getKeyStroke("LEFT"), "left arrow");
+	    inputMap.put(KeyStroke.getKeyStroke("RIGHT"), "right arrow");
+
+	    inputMap = gridPanel.getInputMap(JPanel.WHEN_FOCUSED);
+	    inputMap.put(KeyStroke.getKeyStroke("UP"), "up arrow");
+	    inputMap.put(KeyStroke.getKeyStroke("DOWN"), "down arrow");
+	    inputMap.put(KeyStroke.getKeyStroke("LEFT"), "left arrow");
+	    inputMap.put(KeyStroke.getKeyStroke("RIGHT"), "right arrow");
+	    inputMap.put(KeyStroke.getKeyStroke("Z"), "zoom");
+	    inputMap.put(KeyStroke.getKeyStroke("X"), "dezoom");
+
+
+	    gridPanel.getActionMap().put("up arrow", 
+	    		 new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Enter pressed
+            	System.out.println("Enter pressed!");
+            	panel.yLocaitonShift -= 100;
+//            	panel.calcHypno();
+////            	panel.clearGui();
+//            	panel.UpdateGUI();
+//            	panel.repaint();
+            	panel.clearGui();
+            }});
+	    gridPanel.getActionMap().put("down arrow", 
+	    		 new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Enter pressed
+            	System.out.println("Enter pressed!");
+            	panel.yLocaitonShift += 100;
+//            	panel.calcHypno();
+////            	panel.clearGui();
+//            	panel.UpdateGUI();
+//            	panel.repaint();
+            	panel.clearGui();
+            }});
+	    gridPanel.getActionMap().put("left arrow", 
+	    		 new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Enter pressed
+            	System.out.println("Enter pressed!");
+            	panel.xLocationShift -= 100;
+//            	panel.calcHypno();
+////            	panel.clearGui();
+//            	panel.UpdateGUI();
+//            	panel.repaint();
+            	panel.clearGui();
+            }});
+	    gridPanel.getActionMap().put("right arrow", 
+	    		 new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Enter pressed
+            	System.out.println("Enter pressed!");
+            	panel.xLocationShift += 100;
+//            	panel.calcHypno();
+////            	panel.clearGui();
+//            	panel.UpdateGUI();
+//            	panel.repaint();
+            	panel.clearGui();
+            }});
+	    gridPanel.getActionMap().put("zoom", 
+	    		 new AbstractAction() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               // Enter pressed
+           	System.out.println("zoom");
+//           	
+//            double oldWidth = panel.Scale * 0.10;
+//            double oldHeight = panel.Scale * 0.10;
+//            
+//            double newWidth =   	panel.Scale* 0.10;
+//            double newHeight =  	panel.Scale * 0.10;
+//
+//            // Calculate the difference (and divide it by 2)
+//            double difWidth = (1000) / 2;
+//            double difHeight = (1000/2) / 2;
+            double oldScale =   panel.Scale ;
+            panel.Scale +=100*scaleFactor*2;
+        	double amountScaled = (double) (1000/panel.Scale);
+           	System.out.println("amount scaled :" + (panel.Scale - oldScale/2));
+//            panel.xLocationShift+=(panel.Scale - oldScale)/2;
+//            panel.yLocaitonShift+=(panel.Scale - oldScale)/2;
+////           	panel.calcHypno();
+////           	panel.clearGui();
+//           	panel.UpdateGUI();
+//           	panel.repaint();
+           	panel.clearGui();
+           }});
+
+	    gridPanel.getActionMap().put("dezoom", 
+	    		 new AbstractAction() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               // Enter pressed
+           	System.out.println("dezoom");
+            double oldScale =   panel.Scale ;
+            panel.Scale -=100*scaleFactor*2;
+        	double amountScaled = (double) (1000/panel.Scale);
+           	System.out.println("amount scaled :" + (panel.Scale - oldScale/2));
+//            panel.xLocationShift+=(panel.Scale - oldScale)/2;
+//            panel.yLocaitonShift+=(panel.Scale - oldScale)/2;
+          
+//           	panel.calcHypno();
+////           	panel.clearGui();
+//           	panel.UpdateGUI();
+//           	panel.repaint();
+           	panel.clearGui();
+           }});
+
+	}
 	private void Loop() {
-		while (true){
-			if(graphicsLastUpdateTime + timeBetweenGraphicsUpdate > System.currentTimeMillis()) {
-				panel.UpdateGUI();
-				panel.repaint();
-			}
-		}
+//		while (true){
+//			if(graphicsLastUpdateTime + timeBetweenGraphicsUpdate > System.currentTimeMillis()) {
+////				panel.UpdateGUI();
+////				panel.repaint();
+//				
+//			}
+//		}
 	}
 }
--- src/ca/shahan/graphics/ViewDisplay.java
+++ src/ca/shahan/graphics/ViewDisplay.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Objects;
 
 import javax.swing.JPanel;
 
@@ -13,6 +15,7 @@ public class ViewDisplay extends JPanel{
 	private static final long serialVersionUID = -6080146226481701265L;
 	Color[][] cells =  new Color[1000][1000];
 	public static Color backgroundColor = Color.WHITE;
+	public HashMap<XYPAIR,Color> savedColors = new HashMap<XYPAIR,Color>(30000000); 
 	Image img ;//Maybe buffred?
 	
 	public ViewDisplay(int width, int height) {
@@ -35,14 +38,31 @@ public class ViewDisplay extends JPanel{
 	
 	public void paintComponent(Graphics g){
 		g.drawImage(img, 0, 0, this);
+		System.out.println("Drawing");
 	}
-	
+	public void clearGui() {
+//		cells = new Color[1000][1000];
+//		img = new BufferedImage(1000,1000,BufferedImage.TYPE_INT_ARGB);
+//		for(int i = 0 ; i < cells[0].length;i++) {
+//			for(int j = 0; j<cells.length; j++) {
+//				cells[i][j] = Color.blue;
+//				
+//			}
+//		}
+		calcHypno();
+		UpdateGUI();
+		repaint();
+//		calcHypno();
+		
+	    
+	}
 	public void UpdateGUI() {
 		//  use when u want to clear g.clearrect
+		System.out.println("UPDATING");
 		Graphics g = img.getGraphics();
 		((Graphics2D) g).setBackground(backgroundColor);
-		
-		
+//		g.clearRect(0, 0,  Main.width, Main.height );
+//		g.setColor(Color.black);
 		int cellWidth = Main.width / cells.length ;
 		int cellHeight = Main.height / cells[0] .length;//MIGHT HAVE MixED THESE UP
 		for(int i = 0 ; i < cells[0].length;i++) {
@@ -50,49 +70,69 @@ public class ViewDisplay extends JPanel{
 				
 				
 				g.setColor(cells[i][j]);
-				g.fillRect(i*cellWidth,
-						
+				g.fillRect(i*cellWidth,				
 						j*cellHeight,
 						
 						cellWidth,
 						
-						cellHeight);
-				
+						cellHeight);	
 			}
 		}
+		g.setColor(Color.red);
+		g.fillRect(500, 500, 1, 1);
 
 	}
 	
 	
 	
 	
-	public boolean[][] hypo = new boolean[1000][1000];
-	double number = 1000;
-	double between = 4/number; // 4/length
-
-	double shift = number/2;
+//	public boolean[][] hypo = new boolean[1000][1000];
+	public double Scale = 1000;
+	public double OriginalScale = 1000;
+	// 4/length
+   public int xLocationShift = 1;
+   public int yLocaitonShift = 1;
+	
 	public void calcHypno() {
-		for(int i = 0; i < hypo[0].length; i++) {
-			for(int j = 0; j< hypo.length; j++) {
-				double xM = between * (i - shift);
-				double yM = between * (j - shift);
-				double mandelConst = Math.pow(xM, 2) + Math.pow(yM, 2);
-				int numbers = isMandelbrot(0, 0,xM,yM);
+		double between = 4/Scale;
+		double shift = (Scale/2);
+		System.out.println("CALCUTAITM with scale" + Scale + "Scale Amount = " + Scale/OriginalScale);
+		double xM2 = between * (0+(xLocationShift*Scale/OriginalScale) - shift);
+		double yM2 = between * (0+(yLocaitonShift*Scale/OriginalScale) - shift);
+		System.out.println(xM2 + " " + yM2);
+		for(int i = 0; i < 1000; i++) {
+			for(int j = 0; j< 1000; j++) {
+				double xM = between * (i+(xLocationShift*Scale/OriginalScale) - shift);
+				double yM = between * (j+(yLocaitonShift*Scale/OriginalScale) - shift);
+//				double mandelConst = Math.pow(xM, 2) + Math.pow(yM, 2);
 				
-				if(numbers == -1) {
-//					System.out.println(number);
-//					hypo[i][j] = true;//I KNOW THAT THIS IS UNNECCECRY BUT ITS NEEDED LATER
-					cells[i][j] = Color.BLACK;
+				Color savedColor = savedColors.get (new XYPAIR(xM, yM));
+//				System.out.println(savedColor);
+				if(savedColor==null) {
+					
+					int numbers = isMandelbrot(0, 0,xM,yM);
+
+					if(numbers == -1) {
+						//					System.out.println(number);
+						//					hypo[i][j] = true;//I KNOW THAT THIS IS UNNECCECRY BUT ITS NEEDED LATER
+						cells[i][j] = Color.BLACK;
+					}else {
+						int colorNumber = numbers;
+						int r = 255- (int) Math.max(0d, Math.min(255d, colorNumber * 5));
+						int g = 255- (int) Math.max(0d, Math.min(255d, colorNumber * 5));
+						int b = 255- (int) Math.max(0d, Math.min(255d, colorNumber * 7));
+						//					Color c = Color.WHITE;
+						//					cells[i][j]  =MapColor(colorNumber, xM, yM);
+						Color c = new Color(r,0,b);
+						cells[i][j] = c;
+						savedColors.put(new XYPAIR(xM, yM),c);
+						//					hypo[i][j] = false;
+					} 
+					//				System.out.println(mand el);
 				}else {
-					int colorNumber = numbers;
-					int r = 255- (int) Math.max(0d, Math.min(255d, colorNumber * 5));
-					int g = 255- (int) Math.max(0d, Math.min(255d, colorNumber * 5));
-					int b = 255- (int) Math.max(0d, Math.min(255d, colorNumber * 5d));
-					Color c = Color.WHITE;
-					cells[i][j] = new Color(r,g,b);
-//					hypo[i][j] = false;
-				} 
-//				System.out.println(mand el);
+//					System.out.println("using saved colro");
+					cells[i][j] = savedColor;
+				}
 			}
 		}
 	}
@@ -100,6 +140,23 @@ public class ViewDisplay extends JPanel{
 	
 	int currentCount = 0;//use this to instead get diffrene colors
 	
+	 private Color MapColor(int i, double r, double c){
+//		 System.out.println("Mapping color " + r + " " + c);
+     double di=(double )i;
+     double zn;
+     double hue; 
+
+         zn = Math.sqrt(r + c);
+         hue = di + 1.0 - Math.log(Math.log(Math.abs(zn))) / Math.log(2.0);  // 2 is escape radius
+         hue = 0.95 + 20.0 * hue; // adjust to make it prettier
+         // the hsv function expects values from 0 to 360
+         while (hue >= 360.0)
+             hue -= 360.0;
+         while (hue <= 0.0)
+             hue += 360.0;
+
+         return Color.getHSBColor((float)hue, 0.8f, 1.0f);
+     }
 //	public boolean isMandelbrot(double constNum, double currentnum) {
 //		double value = Math.pow(currentnum, 2) + constNum;
 ////		double value =  Math.pow(currentnum, 2d) + constNum;
@@ -130,11 +187,11 @@ public class ViewDisplay extends JPanel{
 	
 	
 ////	}
-	final int maxCount = 100;
+	final int maxCount = 255;
 	
 	public int isMandelbrot(double a, double b, double c1, double c2) {//-1 means in madel
-		double newA = a*a - b*b + c1; // idk complex number calcuations
-		double newB = 2*a*b + c2;// complex number calcuations
+		double newA = a*a - b*b + c1; 
+		double newB = 2*a*b + c2;
 //		System.out.println("" + newA + " " + newB);
 		for(int i = 0; i < maxCount; i++) {
 			double oldA = newA;
@@ -157,4 +214,21 @@ public class ViewDisplay extends JPanel{
 	    	
 		
 	}
+}
+
+class XYPAIR{
+	double x;
+	double y;
+	public XYPAIR(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	@Override    
+	public boolean equals(Object o) {        
+		return ((XYPAIR)o).x == x && ((XYPAIR)o).y == y;
+	}    
+	@Override    
+	public int hashCode() {        
+		return Objects.hash(x,y);
+	}    
 }
